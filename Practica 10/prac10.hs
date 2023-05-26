4207e05783e15228d55a8b5ce88e043a704d9a6f
martinmerlo0 <dmmb2016v72@gmail.com>
2023-05-25 23:30:43 -0300

    Add ejerc 9

--- Practica 10/prac10.hs
+++ Practica 10/prac10.hs
@@ -20,6 +20,38 @@ iguales' z (x:xs) = (z==x) && iguales' z xs
 
 
 
+{-
+* Ejercicio 5. Derivar una funci ́on que dada una lista determina si los elemen-
+tos est ́an ordenados de forma creciente.
+-}
+
+creciente :: (Ord a) => [a] -> Bool
+creciente [] = True
+creciente [x] = True
+creciente (x:xs) = x <= head xs && creciente xs
+
+
+
+
+
+
+
+
+{-
+Ejercicio 6. Sea m : [Num] → [Num] una funci ́on que devuelve el m ́ınimo de
+una lista dada. Especificar y derivar m.
+-}
+
+minimo :: [Int] -> Int
+minimo [] = 100
+minimo (x:xs) = min x (minimo xs)
+
+
+
+
+
+
+
 
 {-
 Ejercicio 7. Especificar y derivar una funci ́on que dada una lista determine si
@@ -35,3 +67,23 @@ elemSum (x:xs) = elemSum' 0 (x:xs)
 elemSum' :: (Eq a,Num a) => a -> [a] -> Bool
 elemSum' n [] = False
 elemSum' n (x:xs) = (x == (n + sum xs)) || elemSum' (n+x) xs
+
+
+
+{-
+Ejercicio 9. Derivar un programa usando la siguiente especificaci ́on:
+P xs.ys = (∃as, bs :: ys = as + +xs + +bs),
+que dadas dos listas determina si la primera es subsegmento de la segunda.
+-}
+
+subsegmento :: (Eq a)=>[a] -> [a] -> Bool 
+subsegmento [] ys = True 
+subsegmento (x:xs) [] = False
+subsegmento (x:xs) (y:ys) = (y == x) && q xs ys || subsegmento (x:xs) ys
+
+
+
+q :: (Eq a) => [a] -> [a] -> Bool
+q [] ys = True
+q (x:xs) [] = False 
+q (x:xs) (y:ys) = (y == x) && q xs ys 
